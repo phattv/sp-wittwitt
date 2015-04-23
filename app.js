13da448b9efe2dbb2277a4ecdc1d17659dafa1ab
Phat (Austin) Tran <vnaphat.tran93@gmail.com>
2015-04-24 02:00:56 +0700

    Add Update functionality

--- app.js
+++ app.js
@@ -22,9 +22,20 @@ if (Meteor.isClient) {
             };
         }]);
 
-    angular.module('wittwitt').controller('catalogDetailController', ['$scope', '$stateParams',
-        function($scope, $stateParams) {
+    angular.module('wittwitt').controller('catalogDetailController', ['$meteor', '$scope', '$stateParams',
+        function($meteor, $scope, $stateParams) {
+
             $scope.catalogId = $stateParams.id;
+
+            $scope.catalog = $meteor.object(Catalogs, $stateParams.id, false);
+
+            $scope.save = function() {
+                $scope.catalog.save();
+            };
+
+            $scope.cancel = function() {
+                $scope.catalog.reset();
+            }
         }]);
 
     angular.module("wittwitt").config(['$urlRouterProvider', '$stateProvider', '$locationProvider',
